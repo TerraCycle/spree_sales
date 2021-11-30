dc0f6f6d2c8c559b7e73d4abde64b28f990d83af
Kris Birov <kris.birov@gmail.com>
2021-11-30 10:56:48 +0100

    Add locale to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Spree::Core::Engine.routes.draw do
-  namespace :admin do
-    resources :products do
-      resources :sale_prices, only: [:index, :create, :destroy]
+  scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
+    namespace :admin do
+      resources :products do
+        resources :sale_prices, only: [:index, :create, :destroy]
+      end
     end
-  end
 
-  get '/sale',        to: 'home#sale', as: :sale
-  get '/sale/t/*id/', to: 'home#sale', as: :sale_taxon
+    get '/sale',        to: 'home#sale', as: :sale
+    get '/sale/t/*id/', to: 'home#sale', as: :sale_taxon
+  end
 end
